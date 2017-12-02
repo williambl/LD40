cfaed502f4619e6f0f8f33150970da2b9e61c572
williambl <william@williambl.com>
2017-12-02 17:31:15 +0000

    Improved block creation

--- LD40/Assets/Scripts/CreateBlocks.cs
+++ LD40/Assets/Scripts/CreateBlocks.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class CreateBlocks : MonoBehaviour {
 
         public GameObject[] prefabArray;
+        public Vector3 instantiationPosition = new Vector3(0, 5, 0);
+        Quaternion instantiationRotation = Quaternion.Euler(Vector3.zero);
         Rigidbody lastCreated;
 
 	// Use this for initialization
 	void Start () {
-            lastCreated = Instantiate(prefabArray[Random.Range(0,prefabArray.Length)]).GetComponent<Rigidbody>();
+            lastCreated = Instantiate(prefabArray[Random.Range(0, prefabArray.Length)], instantiationPosition, instantiationRotation).GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	    if (lastCreated.IsSleeping())
-                lastCreated = Instantiate(prefabArray[Random.Range(0,prefabArray.Length)]).GetComponent<Rigidbody>();
+                lastCreated = Instantiate(prefabArray[Random.Range(0, prefabArray.Length)], instantiationPosition, instantiationRotation).GetComponent<Rigidbody>();
 	}
 }

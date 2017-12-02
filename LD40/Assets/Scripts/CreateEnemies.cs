3b1f70b95ec8ea51c992f1faeecd866bfa2cd0bb
williambl <william@williambl.com>
2017-12-02 18:45:35 +0000

    Added enemies which try and get across

---
+++ LD40/Assets/Scripts/CreateEnemies.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateEnemies : MonoBehaviour {
+
+        public GameObject enemy;
+
+        Vector3 pos = new Vector3(8.5f, -2.5f, 0f);
+        Quaternion rot = Quaternion.Euler(Vector3.zero);
+
+	// Use this for initialization
+	void Start () {
+	    InvokeRepeating("Create", 0f, 2f);
+	}
+	
+	void Create () {
+	    Instantiate(enemy, pos, rot);	
+	}
+}
---
+++ LD40/Assets/Scripts/Move.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move: MonoBehaviour {
+
+        CharacterController control;
+        public Vector3 speed;
+
+	// Use this for initialization
+	void Start () {
+	    control = GetComponent<CharacterController>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+            control.SimpleMove(speed);
+	}
+}

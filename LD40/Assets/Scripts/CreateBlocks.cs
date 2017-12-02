cb9beb06c1987e9db58657110830681e99a730da
williambl <william@williambl.com>
2017-12-02 17:10:17 +0000

    Made a block maker

---
+++ LD40/Assets/Scripts/CreateBlocks.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateBlocks : MonoBehaviour {
+
+        public GameObject[] prefabArray;
+
+	// Use this for initialization
+	void Start () {	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    GameObject block = Instantiate(prefabArray[0]);
+	}
+}

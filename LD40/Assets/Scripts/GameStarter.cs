1a4a25917bc15877ac1cfaa2f5c1cd9b43a6b610
williambl <william@williambl.com>
2017-12-03 17:35:12 +0000

    Added a menu screen

---
+++ LD40/Assets/Scripts/GameStarter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameStarter : MonoBehaviour {
+
+        public Button startButton;
+
+	// Use this for initialization
+	void Start () {
+            startButton.onClick.AddListener(StartGame);
+	}
+	
+        void StartGame () {
+            SceneManager.LoadScene("main");
+        }
+}
